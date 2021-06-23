fe8164f660367537e4aceb27bba7aae418a07ece
smnkrisz <simonkrisztian993@gmail.com>
2021-06-23 20:57:16 +0200

    drawing

--- week-02/day-03/diagonals-ts.ts
+++ week-02/day-03/diagonals-ts.ts
@@ -1,5 +1,3 @@
-import { Console } from "console";
-
 const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
 const ctx = canvas.getContext('2d');
 export {};
@@ -14,26 +12,21 @@ export {};
 let canvasWidth: number = 600;
 let canvasHeight: number = 400;
 
-let topLeft: number[] = [0, 0];
-let bottomLeft: number[] = [0, canvasHeight];
-let topRight: number[] = [canvasWidth, 0];
-let bottomRight: number[] = [canvasWidth, canvasHeight];
+function singleLine(xStart ,yStart ,xEnd ,yEnd ){
 
-ctx.strokeStyle = 'green';
-ctx.strokeStyle = 'red';
 
-function singleLine(starts: number[], ends: number[]) {
-    if (starts === topLeft && ends === bottomRight) {
-        ctx.strokeStyle = 'green';
-    }   else { starts === topRight && ends === bottomLeft; 
-        ctx.strokeStyle = 'red'
-    }
-    ctx.beginPath();
-    ctx.moveTo(starts[0],starts[1]);
-    ctx.lineTo(ends[0], ends[1]);
+    if ( xStart == 0 && yStart == 0 ){
+         ctx.strokeStyle = 'green'
+    } else  ctx.strokeStyle = 'red';
+
+    ctx.moveTo(xStart, yStart);
+    ctx.lineTo(xEnd, yEnd);
     ctx.stroke();
-}
-singleLine(topLeft, bottomRight);
 
-// the red way:
-singleLine(topRight, bottomLeft);
+
+}
+
+singleLine(0,0,canvasWidth,canvasHeight);
+
+// red way:
+// singleLine(canvasWidth, canvasHeight, 0, 0);
