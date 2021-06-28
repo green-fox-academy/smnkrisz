135479fb4f1aad4115d9eb3a9ab6580249907eee
smnkrisz <simonkrisztian993@gmail.com>
2021-06-28 17:26:38 +0200

    needed to change height value in html to work

--- week-02/day-05/line-play-ts.ts
+++ week-02/day-05/line-play-ts.ts
@@ -6,8 +6,8 @@ export {};
 
 // Draw Line play for 1XP as per: https://github.com/green-fox-academy/teaching-materials/blob/master/project/drawing/r1.png
 
-let canvasWidth: number = 400;
-let canvasHeight: number = 400;
+let canvasWidth: number = 600;
+let canvasHeight: number = 600;
 
 let spaceBetweenLines: number = 20;
 
@@ -20,5 +20,9 @@ function lines(startX: number, startY: number, endX: number, endY:number, color:
 }
 
 for (let i = 0; i < 80; i++){
-    lines(0, i * spaceBetweenLines, i * spaceBetweenLines, canvasHeight, 'green');
+    lines(0, spaceBetweenLines * i, spaceBetweenLines * i, canvasHeight, 'green');
+}
+
+for (let i = 0; i < 80; i++){
+    lines(spaceBetweenLines * i, 0, canvasHeight, spaceBetweenLines * i, 'purple');
 }
