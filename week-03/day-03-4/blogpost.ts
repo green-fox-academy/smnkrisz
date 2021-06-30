12ce48d147e4c25910dd5416bebe0af1e2073c85
smnkrisz <simonkrisztian993@gmail.com>
2021-06-30 20:03:58 +0200

    finished

--- week-03/day-03-4/blogpost.ts
+++ week-03/day-03-4/blogpost.ts
@@ -12,23 +12,29 @@
 // Daniel Hanley, a cybersecurity engineer at IBM, doesn’t want to be the center of attention. When I asked to take his picture outside one of IBM’s New York City offices,
 //he told me that he wasn’t really into the whole organizer profile thing.
 
-import { title } from "process";
+let authorName1: string = 'John Doe';
+let authorName2: string = 'Tim Urban';
+let authorName3: string = 'William Turton';
+
+let title1: string = 'Lorem Ipsum';
+let title2: string = 'Wait but why';
+let title3: string = 'One Engineer Is Trying to Get IBM to Reckon With Trump';
 
 let text1: string = 'Lorem ipsum dolor sit amet.';
 let text2: string = 'A popular long-form, stick-figure-illustrated blog about almost everything.';
 let text3: string = 'Daniel Hanley, a cybersecurity engineer at IBM, doesn’t want to be the center of attention. When I asked to take his picture outside one of IBM’s New York City offices, he told me that he wasn’t really into the whole organizer profile thing.';
 
-let authorName1: string = 'John Doe';
-let authorName2: string = 'Tim Urban';
-let authorName3: string = 'William Turton';
+let publicationDate1: string = '2000.05.04.';
+let publicationDate2: string = '2010.10.10.';
+let publicationDate3: string = '2017.03.28.';
 
 class BlogPost {
     authorName: string;
     title: string;
     text: string;
-    publicationDate: number;
+    publicationDate: string;
     
-    constructor (authorName: string, title: string, text: string, publicationDate: number){
+    constructor (authorName: string, title: string, text: string, publicationDate: string){
         this.authorName = authorName;
         this.title = title;
         this.text = text;
@@ -41,4 +47,12 @@ class BlogPost {
 
 }
 
-let BlogPost1 = new BlogPost(authorName1, title)
+let blogPost1 = new BlogPost(authorName1, title1, text1, publicationDate1);
+let blogPost2 = new BlogPost(authorName2, title2, text2, publicationDate2);
+let blogPost3 = new BlogPost(authorName3, title3, text3, publicationDate3);
+
+blogPost1.show();
+blogPost2.show();
+blogPost3.show();
+
+
