cf043195556790278663d604b99b2478c4bfe6d5
smnkrisz <simonkrisztian993@gmail.com>
2021-07-01 16:08:11 +0200

    fleet of things

---
+++ week-03/day-03-4/fleetOfthings.ts
@@ -0,0 +1,39 @@
+import { Thing } from './thing';
+import { Fleet } from './fleet';
+
+let fleet = new Fleet();
+
+// -  You have the `Thing` class
+// -  You have the `Fleet` class
+// -  Download those, use those
+
+//  Crete a fleet of things to have this output:
+// 1. [ ] Get milk
+// 2. [ ] Remove the obstacles
+// 3. [x] Stand up
+// 4. [x] Eat lunch
+
+let thing1: Thing = new Thing('Get milk');
+let thing2: Thing = new Thing('Remove the obstacles');
+let thing3: Thing = new Thing('Stand up');
+let thing4: Thing = new Thing('Eat lunch');
+
+thing1.getName();
+thing2.getName();
+thing3.getName();
+thing4.getName();
+
+fleet.add(thing1);
+fleet.add(thing2);
+fleet.add(thing3);
+fleet.add(thing4);
+
+thing3.complete();
+thing4.complete();
+
+thing1.toString();
+thing2.toString();
+thing3.toString();
+thing4.toString();
+
+console.log(fleet.toString());
