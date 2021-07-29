2aca1c25d579c9b926027081634de923b6fdef7a
smnkrisz <simonkrisztian993@gmail.com>
2021-07-29 17:20:18 +0200

    create templating hello-world

---
+++ week-06/day-04/temp-hello-world/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// home page
+app.get('/', (req, res) => {
+  // render `home.ejs`
+  res.render('home');
+});
+
+// start express app on port 3000
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
