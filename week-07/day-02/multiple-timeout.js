aa2c17fb87673adc708d6b55f41323408954d3f4
smnkrisz <simonkrisztian993@gmail.com>
2021-08-03 18:06:46 +0200

    multiple-timeout exercise

---
+++ week-07/day-02/multiple-timeout.js
@@ -0,0 +1,21 @@
+'use strict';
+
+// Write a program that prints the following fruits:
+
+const printingFruits = () => {
+// apple -> immediately
+console.log('apple');
+// pear -> after 1 seconds
+setTimeout(() => {
+    console.log('pear');
+}, 1000);
+// melon -> after 3 seconds
+setTimeout(() => {
+    console.log('melon');
+}, 3000);
+// grapes -> after 5 seconds
+setTimeout(() => {
+    console.log('grapes');
+}, 5000);
+};
+printingFruits();
