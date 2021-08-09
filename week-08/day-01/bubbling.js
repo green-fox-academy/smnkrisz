138a90ffd7a3c7dee0b3bd54927d37eb134f2801
smnkrisz <simonkrisztian993@gmail.com>
2021-08-09 17:39:03 +0200

    bubbling exercise skeleton

---
+++ week-08/day-01/bubbling.js
@@ -0,0 +1,6 @@
+const button = document.querySelector('button');
+const imgInspector = document.querySelector('.img-inspector');
+const coordinateX = 0;
+const coordinateY = 0;
+const backgroundSize = 200;
+
