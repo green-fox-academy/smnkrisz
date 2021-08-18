37046d768885706fbf2084afcb5ba9c026e985a9
smnkrisz <simonkrisztian993@gmail.com>
2021-08-18 20:19:07 +0200

    quiz app structure and added get-post requests

---
+++ week-09/day-03-4/backend/server.js
@@ -0,0 +1,82 @@
+import express from 'express'
+import mysql from 'mysql2'
+import dotenv from 'dotenv'
+import cors from 'cors'
+
+const app = express()
+const PORT = 8080
+
+dotenv.config()
+app.use(express.json())
+app.use(cors())
+
+app.use('/game', express.static('frontend/game.html'))
+app.use('/questions', express.static('frontend/questions.html'))
+
+const db = mysql.createConnection({
+    host: process.env.KRSZ_HOST,
+    user: process.env.KRSZ_USER,
+    password: process.env.KRSZ_PSSWRD,
+    database: process.env.KRSZ_DB
+})
+
+db.connect(err => {
+    if (err) {
+        console.log('Cannot connect to database.');
+    } else {
+        console.log('Connection established.');
+    }
+});
+
+app.get('/api/game', (req, res) => {
+    db.query('SELECT * FROM questions;', (err, result) => {
+        let questionLength = result.length
+        let randomQ = Math.floor(Math.random() * questionLength)
+        if (err) {
+            console.log(err)
+            res.send(404)
+        } else {
+            res.status(200).json(result[randomQ])
+        }
+    })
+})
+
+app.get('/api/questions', (req, res) => {
+    db.query('SELECT * FROM questions;', (err, result) => {
+        if (err) {
+            console.log(err)
+            res.send(404)
+        } else {
+            res.status(200).json(result)
+        }
+    })
+})
+
+app.post('/api/questions', async (req, res) => {
+    let question = req.body.question
+    let answers = req.body.answers
+
+    db.query('INSERT INTO questions(question) VALUES (?);', [question], (err, result) => {
+        if (err) {
+            console.log(err)
+            res.send(404)
+        } else {
+            answers.foreach(async (element, index) => {
+                try {
+                    let answerString = `answer_${index + 1}`
+                    await db.promise().query('INSERT INTO answers (answer, question_id, is_correct) VALUES (?, ?, ?)', [element[answerString], result.insertId, element.is_correct], (err, result) => {
+                        if (err) {
+                            console.log(err)
+                            res.send(404)
+                        }
+                    })
+                } catch (error) {
+                    console.log(error)
+                }
+            })
+            res.status(200).json({ insert: "OK" })
+        }
+    })
+})
+
+app.listen(PORT);
