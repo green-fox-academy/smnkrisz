b18433562dc9a313d14c7cc19964abe9ed072215
smnkrisz <simonkrisztian993@gmail.com>
2021-08-18 15:54:52 +0200

    different structure

--- week-08/day-05/reddit/frontend/index.js
+++ week-08/day-05/reddit/frontend/index.js
@@ -9,34 +9,41 @@ window.onload = () => {
             const responseObject = response.posts
 
             responseObject.forEach(post => {
-                const div = document.querySelector('.posts')
-                const newDiv = document.createElement('div')
-                div.appendChild(newDiv)
-
-                const newTitle = document.createElement('h2')
-                newTitle.innerHTML = post.title
-                newDiv.appendChild(newTitle)
-
-                const newURL = document.createElement('a')
-                newURL.innerHTML = post.url
-                newDiv.appendChild(newURL)
-
-                const newTime = document.createElement('p')
-                newTime.innerHTML = convertEpoch(post.timestamp)
-                newDiv.appendChild(newTime)
-
-                const newScore = document.createElement('p')
-                newScore.innerHTML = post.score
-                newDiv.appendChild(newScore)
-
-                const newModify = document.createElement('a')
-                newModify.innerHTML = "modify"
-                newDiv.appendChild(newModify)
-
-                const newRemove = document.createElement('a')
-                newRemove.innerHTML = "remove"
-                newDiv.appendChild(newRemove)
-
+                const posts = document.getElementsByClassName('posts')[0]
+                posts.appendChild(createPosts(post))
             })
         })
+}
+
+const createPosts = object => {
+
+    const newDiv = document.createElement('div')
+    newDiv.className = 'posts'
+
+    const newTitle = document.createElement('h2')
+    newTitle.innerHTML = object.title
+
+    const newURL = document.createElement('a')
+    newURL.innerHTML = object.url
+
+    const newTime = document.createElement('p')
+    newTime.innerHTML = convertEpoch(object.timestamp)
+
+    const newScore = document.createElement('p')
+    newScore.innerHTML = object.score
+
+    const newModify = document.createElement('a')
+    newModify.innerHTML = "modify"
+
+    const newRemove = document.createElement('a')
+    newRemove.innerHTML = "remove"
+
+    newDiv.appendChild(newTitle)
+    newDiv.appendChild(newURL)
+    newDiv.appendChild(newTime)
+    newDiv.appendChild(newScore)
+    newDiv.appendChild(newModify)
+    newDiv.appendChild(newRemove)
+
+    return newDiv
 }
